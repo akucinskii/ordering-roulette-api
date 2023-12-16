4beab74c3c4ee01f206c0e01cd754e5c793bb0a6
Artur Kucinski <artuyees@gmail.com>
2023-12-16 12:19:25 +0100

    Add SQLite database functionality and lottery endpoints

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,38 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import sqlite3 from 'sqlite3';
+import { Database, open } from 'sqlite';
 
 const app = express();
 app.use(cors());
 const httpServer = http.createServer(app);
+
+const initializeDb = async (): Promise<
+  Database<sqlite3.Database, sqlite3.Statement>
+> => {
+  const db = await open({
+    filename: 'mydb.sqlite',
+    driver: sqlite3.Database,
+  });
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY, 
+      username TEXT, 
+      room TEXT
+    );
+    CREATE TABLE IF NOT EXISTS lotteries (
+      id INTEGER PRIMARY KEY, 
+      date TEXT, 
+      room TEXT, 
+      participants TEXT, 
+      winner TEXT
+    );
+  `);
+
+  return db;
+};
 
 const io = new Server(httpServer, {
   cors: {
@@ -18,49 +46,134 @@ const PORT = process.env.PORT || 3000;
 
 const roomUserMap = {};
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+type Lottery = {
+  date: string;
+  room: string;
+  participants: string;
+  winner: string;
+};
 
-  const sendRoomUserList = (roomName: string): void => {
-    const userList = Array.from(roomUserMap[roomName] || []);
-    io.to(roomName).emit('updateUserList', userList);
-  };
+const addLotteryToDB = async (
+  db,
+  room,
+  participants,
+  winner,
+): Promise<void> => {
+  const date = new Date().toISOString();
+  await db.run(
+    'INSERT INTO lotteries (date, room, participants, winner) VALUES (?, ?, ?, ?)',
+    [date, room, JSON.stringify(participants), winner],
+  );
+};
 
-  socket.on('requestUserList', (roomName) => {
-    sendRoomUserList(roomName);
-  });
+const getLotteries = async (db): Promise<Lottery[]> => {
+  const lotteries = await db.all('SELECT * FROM lotteries');
+  return lotteries;
+};
 
-  // Join a room
-  socket.on('joinRoom', ({ room, username }) => {
-    socket.join(room);
-    if (!roomUserMap[room]) {
-      roomUserMap[room] = new Set();
-    }
-    roomUserMap[room].add(username);
-    sendRoomUserList(room);
-  });
+const getLotteriesThatContainUser = async (
+  db,
+  username: string,
+): Promise<Lottery[]> => {
+  const lotteries = await db.all(
+    'SELECT * FROM lotteries WHERE participants LIKE ?',
+    `%${username}%`,
+  );
+  return lotteries;
+};
 
-  socket.on('startLottery', (room) => {
-    const roomFromAdapter = io.sockets.adapter.rooms.get(room);
-    const roomSize = roomFromAdapter ? roomFromAdapter.size : 0;
-    const numberOfPartitions = roomSize;
-    const randomNumber = Math.random() * 360;
+const getLottieriesThatUserWon = async (
+  db,
+  username: string,
+): Promise<Lottery[]> => {
+  const lotteries = await db.all(
+    'SELECT * FROM lotteries WHERE winner = ?',
+    username,
+  );
+  return lotteries;
+};
 
-    const winner = Math.ceil(randomNumber / (360 / numberOfPartitions));
+const getRoomUserList = (roomName: string): string[] => {
+  return Array.from(roomUserMap[roomName] || []);
+};
 
-    // Broadcast the winner data to all clients in the specified room
-    io.to(room).emit('winnerData', { winner, randomNumber });
-  });
+const addNewRoom = (roomName: string): void => {
+  if (!roomUserMap[roomName]) {
+    roomUserMap[roomName] = new Set();
+  }
+};
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
+const addNewUserToRoom = (roomName: string, username: string): void => {
+  addNewRoom(roomName);
+  roomUserMap[roomName].add(username);
+};
 
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+initializeDb().then(async (db) => {
+  httpServer.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(`url: http://localhost:${PORT}`);
+  }),
+    io.on('connection', (socket) => {
+      const sendRoomUserList = (roomName: string): void => {
+        const userList = getRoomUserList(roomName);
+        io.to(roomName).emit('updateUserList', userList);
+      };
+
+      socket.on('requestUserList', (roomName) => {
+        sendRoomUserList(roomName);
+      });
+
+      // Join a room
+      socket.on('joinRoom', ({ room, username }) => {
+        socket.join(room);
+
+        addNewRoom(room);
+
+        addNewUserToRoom(room, username);
+
+        sendRoomUserList(room);
+      });
+
+      socket.on('startLottery', async (room) => {
+        const roomFromAdapter = io.sockets.adapter.rooms.get(room);
+        const roomSize = roomFromAdapter ? roomFromAdapter.size : 0;
+        const participants = getRoomUserList(room);
+        const numberOfPartitions = roomSize;
+        const randomNumber = Math.random() * 360;
+
+        const winner = Math.ceil(randomNumber / (360 / numberOfPartitions));
+
+        // Broadcast the winner data to all clients in the specified room
+        await addLotteryToDB(db, room, participants, participants[winner - 1]);
+
+        io.to(room).emit('winnerData', { winner, randomNumber });
+      });
+
+      socket.on('disconnect', () => {
+        // Since we want the user to stay in the room even if they disconnect, we don't remove them from the room
+        console.log('A user disconnected');
+      });
+    });
 });
 
 app.get('/', (_, res) => {
   res.send('Socket.io Lottery Server with Rooms');
+});
+
+app.get('/lotteries', async (_, res) => {
+  const db = await initializeDb();
+  const lotteries = await getLotteries(db);
+  res.send(lotteries);
+});
+
+app.get('/lotteries/:username', async (req, res) => {
+  const db = await initializeDb();
+  const lotteries = await getLotteriesThatContainUser(db, req.params.username);
+  res.send(lotteries);
+});
+
+app.get('/lotteries/won/:username', async (req, res) => {
+  const db = await initializeDb();
+  const lotteries = await getLottieriesThatUserWon(db, req.params.username);
+  res.send(lotteries);
 });
